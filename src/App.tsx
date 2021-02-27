7c8f351d4f8e939f2882686f6a18dc8cfc23f9ea
Yuri Almeida <yurialves2@gmail.com>
2021-02-27 20:12:07 -0300

    chore: added first route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import ListaProdutos from './views/ListaProdutos';
+
+const Stack = createStackNavigator();
+const { Screen, Navigator } = Stack;
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <ListaProdutos />
+      <NavigationContainer>
+        <Navigator initialRouteName="ListaProdutos">
+          <Screen
+            name="ListaProdutos"
+            component={ListaProdutos}
+            options={{ headerShown: false }}
+          />
+        </Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
