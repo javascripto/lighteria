acc6dfa32ce4101d6b33742e3eafff2d2cf5bfb5
Yuri Almeida <yurialves2@gmail.com>
2021-02-28 00:12:47 -0300

    fix: android currency format

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,11 @@
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
+import { Platform } from 'react-native';
 
 export const formataValor = (valor) =>
   new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL',
-  }).format(valor);
+  })
+    .format(valor)
+    .replace(/(\D*)/, Platform.select({ android: '$1 ', ios: '$1' }));
